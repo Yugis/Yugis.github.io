c09cb41605ebf20b0b98c71ac03109546ac9d21e
Yugis <omar.tarek.18@hotmail.com>
2024-08-05 16:18:06 +0400

    Add new article and formatting

--- js/counterScript.js
+++ js/counterScript.js
@@ -3,7 +3,7 @@ const counter = document.querySelector('.counter-number');
 async function updateCounter() {
     let response = await fetch('https://npc62a67a3u2nwq734pqkhi2fq0baike.lambda-url.us-east-1.on.aws/')
     let data = await response.json();
-    counter.innerHTML = data
+    counter.innerHTML = new Intl.NumberFormat().format(data)
 }
 
 updateCounter()
